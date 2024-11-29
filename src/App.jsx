f36612352cf4f746007cf1586a95e5a1fa68ebf9
NganNguyen0308 <nguyenngannn0308@gmail.com>
2024-11-29 15:44:49 +0700

    updated about session

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
-import About from './components/ABout'
+import About from './components/Aboutsession'
 
 export default function App() {
   return (
     <div className='relative min-h-screen w-screen overflow-x-hidden'>
       <Hero/>
-     <About/>
+      <About/>
     </div>
   )
 }
